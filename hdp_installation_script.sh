7f61aff149bbb4caeaa89eb2448366f339a91f1d
Dan Zaratsian <dtz001@gmail.com>
2017-06-29 23:24:41 -0400

    Update hdp_installation_script.sh

--- hdp_installation_script.sh
+++ hdp_installation_script.sh
@@ -7,6 +7,7 @@
 #
 #   Installation: https://docs.hortonworks.com/HDPDocuments/Ambari-2.5.1.0/bk_ambari-installation/content/ch_Getting_Ready.html
 #   Ambari Repos: https://docs.hortonworks.com/HDPDocuments/Ambari-2.5.1.0/bk_ambari-installation/content/ambari_repositories.html
+#   HDP Repos:    https://docs.hortonworks.com/HDPDocuments/Ambari-2.5.1.0/bk_ambari-installation/content/hdp_26_repositories.html
 #   Test on: CentOS Linux release 7.2.1511 (Core) 
 #
 #################################################################################################################
